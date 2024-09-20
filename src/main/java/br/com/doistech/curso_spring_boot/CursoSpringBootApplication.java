ae77bffb36187843ce6752fe2e6ca06ab80a10a3
Brenno Agostini <brenno.agostini@ipremios.com>
2024-09-19 22:12:00 -0300

    setup
    setup

---
+++ src/main/java/br/com/doistech/curso_spring_boot/CursoSpringBootApplication.java
@@ -0,0 +1,13 @@
+package br.com.doistech.curso_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSpringBootApplication.class, args);
+	}
+
+}
